3629a0a23e3bc0309d11aaa1fa91cd37890787cc
dev-737 <73829355+dev-737@users.noreply.github.com>
2022-03-25 15:55:17 +0530

    Updated Userinfo Command

--- commands/info/userinfo.js
+++ commands/info/userinfo.js
@@ -12,8 +12,7 @@ module.exports = {
         
         let user = await client.users.fetch(args[0])
         let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === args[0]) || user || message.member 
-        //const Member = message.guild.members.cache.get(Target.id);
-
+        
         try {
         const responce = new MessageEmbed()
         
@@ -21,11 +20,12 @@ module.exports = {
         .setColor("BLUE")
         .setThumbnail(member.displayAvatarURL({ dynamic : true }))
         .addField('Basic Info', `**ID:** ${member.id || user.id}\n**Roles:** ${member.roles.cache.map(r => r).join(' ').replace('@everyone', '')}\n**Nickname:** ${member.displayName}\n**No. Roles:** ${member.roles.cache.size }`, false)
-        //.addField('User Info: ', `**Joined Server on:** \`${moment(member.joinedAt).format('MMM DD YYYY')}\` (${moment(member.joinedAt).startOf('day').fromNow()}) \n**Joined Discord on:**  \`${moment(member.createdAt).format('MMM DD YYYY')}\` (${moment(member.createdAt).startOf('day').fromNow()})`, true)
-        //.addField('Member Since', `<t:${parseInt(member.joinedTimestamp / 1000)}:R>`, true)
+        
         .addField('Other Info ', `**Member Joined at:** ${moment(member.joinedAt).format('MMM DD YYYY') || "Not In Guild."}  (<t:${parseInt(member.joinedTimestamp / 1000)}:R>)\n **Joined Discord at:** ${moment(member.user.createdAt).format('MMM DD YYYY')} (<t:${parseInt(member.user.createdTimestamp / 1000)}:R>\n**Nitro:** ${member.user.avatar.startsWith('a_') || member.user.premiumSince ? 'Has Nitro' : 'Regular User'}`, true)
         message.channel.send({ embeds : [responce] })
-    } catch {
+    } 
+    
+    catch {
       const responce = new MessageEmbed()
         
         .setAuthor( { name : `${member.username}`, iconURL : member.displayAvatarURL({ dynamic : true }) })
@@ -33,26 +33,15 @@ module.exports = {
         .setThumbnail(member.displayAvatarURL({ dynamic : true }))
         .setDescription(`${member.avatar.startsWith('a_') || member.premiumSince ? '<:nitro:956858085069160449>' : '<:nou:956485288534876160> User has no badges.'}`)
         .addField('User Info', `**ID:** ${member.id || user.id}\n**Joined Discord at:** ${moment(member.createdAt).format('MMM DD YYYY')} (<t:${parseInt(member.createdTimestamp / 1000)}:R>)`, false)
-        //.addField('User Info: ', `**Joined Server on:** \`${moment(member.joinedAt).format('MMM DD YYYY')}\` (${moment(member.joinedAt).startOf('day').fromNow()}) \n**Joined Discord on:**  \`${moment(member.createdAt).format('MMM DD YYYY')}\` (${moment(member.createdAt).startOf('day').fromNow()})`, true)
-        //.addField('Member Since', `<t:${parseInt(member.joinedTimestamp / 1000)}:R>`, true)
+        
         .setTimestamp()
         message.channel.send({ embeds : [responce] })
         
       
     }
-        
-      
-        //   .addField(
-      //   'Nitro',
-      //   member.premiumSinceTimestamp
-      //     ? `Since ${new Date(member.premiumSinceTimestamp).toLocaleDateString()}`
-      //     : 'No.',
-      //   true,
-      // )
 
         
-        
-        
+            
     }
 
     
